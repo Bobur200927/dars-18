e58e7245c1121a7aa7acfad336dd380f5612e05d
Qarshiboyev Bobur <you@example.com>
2024-01-11 14:01:52 +0500

    fcehdwbh

---
+++ script.js
@@ -0,0 +1,126 @@
+
+// ? Vazifa
+
+let car = {
+    model: "Ferari",
+    made: "America",
+    color: "blue",
+}
+
+console.log(car);
+
+
+
+// ? Vazifa 2
+
+let a = 1234567890;
+let b = 1234567890;
+
+console.log(a == b);
+
+
+
+// ? Vazifa 3
+
+let c = 7;
+
+console.log(c);
+
+
+
+// ? Vazifa 4
+
+let d = 1;
+
+console.log(d>0);
+
+
+
+// ? Vazifa 5
+
+let e = 99;
+
+console.log(e<0);
+
+
+
+// ? Vazifa 6
+
+let f = 19;
+
+console.log(f % 2 === 1);
+
+
+
+// ? Vazifa 7
+
+let g = 8;
+
+console.log(g % 2 == 0);
+
+
+
+// ? Vazifa 8
+
+// ==================== || yoki ====================
+// ==================== && va ====================
+
+let h = 3;
+let k = 4;
+let l = 5;
+
+console.log(k>h && k<l);
+
+
+
+// ? Vazifa 9
+
+let m = 3;
+let n = 0;
+
+
+console.log(m>=0 && n>=0);
+
+
+
+// ? Vazifa 10
+
+let o = -2;
+let p = -4;
+
+console.log(o<=0 && p<=0);
+
+
+
+
+// ? Vazifa 11
+
+let q = 6;
+let r = 4;
+
+console.log(q % 2 == 0 && r % 2 ==0);
+
+
+
+
+// ? Vazifa 12
+
+function sayHello(s, t) {
+    let result = s % 2 == 1 && t % 2 ==0;
+
+    return result;
+
+    console.log(sayHello(7,8));
+}
+
+// ? ==================== Function  ====================
+
+let result = 30;
+
+function sayHello(a, b) {
+    let result = a + b * 10;
+
+    return result;
+}
+
+console.log(sayHello(12,34));
